ae5235ee423414bf03078cd43a9a7c60cc7bc618
webdeveloper510 <webdeveloper510@gmail.com>
2022-05-05 20:37:33 +0530

    QA Issues

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -92,7 +92,7 @@
 
     <h3 style="font-family: inherit;font-weight: 400;line-height: 1.5;color: #32325d;font-size: 20px;padding-left: 20px;margin: 0;">Total: @money(round($delivery_price+$subtotal+$discount+$vat_cal,2) , $currency,true)</h3>
 
-               <hr style="margin: 20px;"/>
+               <hr style="margin: 20px;"/>  
 
     
 
